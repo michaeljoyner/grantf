2b196ad71b82ab5e94d71ec9ae3aa60497209359
Michael Joyner <joyner.michael@gmail.com>
2016-05-25 10:59:04 +0800

    schedule newsletter

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('newsletter:issue')->weeklyOn(6, '12:00');
     }
 }
