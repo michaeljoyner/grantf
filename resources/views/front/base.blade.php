207d9cc8a56c9e2e9594e413ea2c477c474db82d
Michael Joyner <joyner.michael@gmail.com>
2016-05-25 11:26:24 +0800

    add favicons

--- resources/views/front/base.blade.php
+++ resources/views/front/base.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @yield('head')
-    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    @include('front.partials.favicons')
     <!-- Place favicon.ico in the root directory -->
 
     <link rel="stylesheet" href="{{ elixir('css/fapp.css') }}">
